c71e85d74728add560ae0e218c0c817fde65c797
Sarah Woodring <Sarah Woodring>
2017-06-20 20:32:49 -0400

    updating app.js to use react router so everyone's work is in a separate (though probably not correctly/finally named) url space

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Welcome to React</h2>
+				</div>
+				<BrowserRouter>
+					<div>
+						<Route exact path="/" render={() => <div>Home screen</div>} />
+						<Route exact path="/ryan" render={() => <div>Hello from Ryan's section!</div>} />
+						<Route exact path="/chris" render={() => <div>Hello from Chris' section`!</div>} />
+						<Route exact path="/kris" render={() => <div>Hello from Kris' section!</div>} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
 export default App;
